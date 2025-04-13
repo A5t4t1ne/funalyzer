a4b0e3f4870afef7a2e7591e525630fbc7c299bb
A5t4t1ne <62953863+A5t4t1ne@users.noreply.github.com>
2025-04-13 16:23:32 +0200

    re-arrange widget registration

--- src/__init__.py
+++ src/__init__.py
@@ -4,12 +4,9 @@ HEADLESS_MODE = False
 try:
     from binaryninjaui import Sidebar
     from .views.sidebar import FunalyzerSidebarWidgetType
+    Sidebar.addSidebarWidgetType(FunalyzerSidebarWidgetType())
 except binaryninja.UIPluginInHeadlessError:
     # We're in a headless environment, so UI modules are not available
     HEADLESS_MODE = True
 
-
-if not HEADLESS_MODE:
-    # Register the sidebar widget
-    Sidebar.addSidebarWidgetType(FunalyzerSidebarWidgetType())
 
