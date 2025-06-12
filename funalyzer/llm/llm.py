1e3c0969eae907df4b8e52fb8beba3d999448a2c
A5t4t1ne <62953863+A5t4t1ne@users.noreply.github.com>
2025-06-12 20:17:36 +0200

    extend MCP

--- funalyzer/llm/llm.py
+++ funalyzer/llm/llm.py
@@ -3,7 +3,7 @@ from binaryninja.log import log_info, log_error
 from google import genai
 from google.genai.types import GenerateContentResponse
 from funalyzer.config.llm_config import Prompts, GEMINI_API_KEY, PromptVars
-# from funalyzer.llm.mcp_client import run
+from funalyzer.llm.mcp_client import run
 from enum import Enum, auto
 from pathlib import Path
 
@@ -16,21 +16,12 @@ class LLM_REQUEST_TYPE(Enum):
     RENAME_VAR = auto()
 
 
-async def llm_request(bv: bn.BinaryView, func: bn.function.Function, req_type: LLM_REQUEST_TYPE, variable: str | None = None) -> str:
+async def llm_request(bv: bn.BinaryView, func: bn.function.Function, req_type: LLM_REQUEST_TYPE, variable: str | None =
+                      None, mcp_path: str = "") -> str:
     target_func_llil_str = ""
-    # all_funcs_str = ""
     for bb in func.low_level_il:
         target_func_llil_str += "\n".join(str(instr) for instr in bb)
 
-
-    # for func in bv.functions:
-    #     try:
-    #         all_funcs_str += f"{func.name}():"
-    #         for bb in func.low_level_il:
-    #             for instr in bb:
-    #                 all_funcs_str += f'0x{instr.address:x}: {instr}\n'
-    #     except Exception as e:
-    #         log_error(e)
 
     client = genai.Client(api_key=GEMINI_API_KEY)
     response: GenerateContentResponse | None = None
@@ -43,11 +34,16 @@ async def llm_request(bv: bn.BinaryView, func: bn.function.Function, req_type: L
             prompt = Prompts.ANALYZE_FUNCTION
             prompt = prompt.replace(str(PromptVars.LLIL_FUNC_CODE), target_func_llil_str)
             prompt = prompt.replace(str(PromptVars.LIBRARY_NAME), '"arm-none-eabi"')
-            # prompt = prompt.replace(PromptVars.LLIL_FILE_CODE, all_funcs_str)
 
-            # server_path = local_path / './gemini_mcp_server.py'
-            # response = await run(server_path)
-            response = client.models.generate_content(model="gemini-2.0-flash", contents=[prompt])
+            if mcp_path != "":
+                try:
+                    server_path = local_path / mcp_path
+                    response = await run(server_path)
+                except Exception as e:
+                    log_error(f"Connection to MCP server failed {e}")
+                    
+            else:
+                response = client.models.generate_content(model="gemini-2.0-flash", contents=[prompt])
             log_info("end task")
         except Exception as e:
             log_error(f"Request to LLM failed: {e}")
--- funalyzer/llm/mcp_client.py
+++ funalyzer/llm/mcp_client.py
@@ -1,14 +1,7 @@
-import asyncio
-from enum import Enum, auto
-from types import CoroutineType
-from typing import List, Optional, Any, Dict
-from contextlib import AsyncExitStack
-from pathlib import Path
-
-from binaryninja.log import log_debug, log_info
+from binaryninja.log import log_info
 from mcp import ClientSession, StdioServerParameters, Tool
 from mcp.client.stdio import stdio_client
-from mcp.types import CallToolResult
+from pathlib import Path
 
 
 async def run(server_path: Path):
@@ -31,75 +24,3 @@ async def run(server_path: Path):
             prompt = "Explain the Model Context Protocol in one sentence."
             result = await session.call_tool("ask_gemini", {"prompt": prompt})
             log_info(f"Gemini says: {result.content}")
-
-
-# class MCPClient:
-#     def __init__(self):
-#         self.session: Optional[ClientSession] = None
-#         self.exit_stack = AsyncExitStack()
-#         self.stdio = None
-#         self.write = None
-# 
-#     async def connect(self, server_path: Path):
-#         """
-#         Connect to an MCP server via stdio.
-#         Args:
-#             server_script_path: Path to the server script (e.g., 'example_server.py')
-#         """
-#         if not server_path.suffix == ".py":
-#             raise ValueError("Server script must be a .py file")
-#         if not server_path.exists():
-#             raise ValueError(f"File {server_path.resolve()} does not exist")
-#         # server_params = StdioServerParameters(
-#         #     command="python",
-#         #     args=[str(server_path)],
-#         #     env=None
-#         # )
-#         log_debug("connecting to MCP server")
-#         proc = await asyncio.create_subprocess_exec(
-#             "python", str(server_path),
-#             stdin=asyncio.subprocess.PIPE,
-#             stdout=asyncio.subprocess.PIPE,
-#         )
-#         self.stdio = proc.stdout
-#         self.write = proc.stdin
-#         self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
-#         # stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_params))
-#         # log_info("here 2")
-#         # self.stdio, self.write = stdio_transport
-#         # self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
-#         log_info("here 3")
-#         await self.session.initialize()
-#         log_info("here 4")
-#         tools = await self.session.list_tools()
-#         log_info("here 5")
-#         log_info(f"Connected to server with tools: {[tool.name for tool in tools.tools]}")
-# 
-#     async def list_tools(self) -> List[Tool]:
-#         """
-#         List available tools from the MCP server.
-#         """
-#         if self.session is None:
-#             raise RuntimeError("Not connected to MCP server")
-#         tools = await self.session.list_tools()
-#         return tools.tools
-# 
-#     async def call_tool(self, tool_name: str, arguments: Dict[str, Any]) -> CallToolResult:
-#         """
-#         Call a tool exposed by the MCP server.
-#         Args:
-#             tool_name: Name of the tool to call.
-#             arguments: Arguments for the tool.
-#         """
-#         if self.session is None:
-#             raise RuntimeError("Not connected to MCP server")
-#         return await self.session.call_tool(tool_name, arguments=arguments)
-# 
-#     async def close(self):
-#         """
-#         Cleanly close the MCP client session and resources.
-#         """
-#         await self.exit_stack.aclose()
-
-
-
